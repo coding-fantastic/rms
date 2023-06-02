089648ee5268cd0886ed133797d95c74f57776e8
coding-fantastic <alexnzioka@protonmail.com>
2023-06-02 18:37:37 +0300

    update on admin

--- core/admin.py
+++ core/admin.py
@@ -6,22 +6,22 @@ from core.models import *
 @admin.register(Building)
 
 class BuildingModel(admin.ModelAdmin):
-    list_filter =('buildingName', 'city' , 'createdAt', 'modifiedAt')
-    list_display = ('buildingName' , 'city', 'createdAt', 'modifiedAt')
+    list_filter =('buildingName', 'city' , 'createdAt')
+    list_display = ('buildingName' , 'city', 'createdAt')
     
 
 @admin.register(Room)
 
 class RoomModel(admin.ModelAdmin):
-    list_filter =('building','floor', 'roomNumber', 'roomStatus','createdAt', 'modifiedAt')
-    list_display = ('building','floor', 'roomNumber', 'roomStatus','createdAt', 'modifiedAt')
+    list_filter =('building','floor', 'roomNumber', 'roomStatus','createdAt')
+    list_display = ('building','floor', 'roomNumber', 'roomStatus','createdAt')
 
 
 @admin.register(Floor)
 
 class FloorModel(admin.ModelAdmin):
-    list_filter =('building','floorNumber','createdAt', 'modifiedAt')
-    list_display = ('building','floorNumber','createdAt', 'modifiedAt')
+    list_filter =('building','floorNumber','createdAt')
+    list_display = ('building','floorNumber','createdAt')
 
 
 @admin.register(Tenant)
@@ -33,13 +33,13 @@ class TenantModel(admin.ModelAdmin):
 @admin.register(Rental)
 
 class RentalModel(admin.ModelAdmin):
-    list_filter =('building','floor', 'room','rentalAmount','createdAt', 'modifiedAt')
-    list_display = ('building','floor', 'room','rentalAmount','createdAt', 'modifiedAt')
+    list_filter =('building','floor', 'room','rentalAmount','createdAt')
+    list_display = ('building','floor', 'room','rentalAmount','createdAt')
 
     
 @admin.register(Comments)
     
 class CommentsModel(admin.ModelAdmin):
-    list_filter =('building','floor', 'room','comment','createdAt', 'modifiedAt')
-    list_display = ('building','floor', 'room','comment','createdAt', 'modifiedAt')
+    list_filter =('building','floor', 'room','comment','createdAt')
+    list_display = ('building','floor', 'room','comment','createdAt')
 

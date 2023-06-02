35bc33c2957c4da7dd5a298859cae5d5955f9ac8
coding-fantastic <alexnzioka@protonmail.com>
2023-06-02 18:15:28 +0300

    unknown path

--- backend/urls.py
+++ backend/urls.py
@@ -26,13 +26,17 @@ from django.views.static import serve
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include , re_path
 from rest_framework.authtoken.views import obtain_auth_token
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('core.urls')),
     path('auth/', obtain_auth_token),
+
+    # Catch-all pattern for undefined URLs
+    re_path(r'^.*/$', RedirectView.as_view(url='/')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve,

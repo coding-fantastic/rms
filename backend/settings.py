0b4e9b0919fd488883d53781d5a9263d8e26141e
coding-fantastic <alexnzioka@protonmail.com>
2023-06-04 15:12:58 +0300

    update

--- backend/settings.py
+++ backend/settings.py
@@ -137,7 +137,8 @@ MEDIA_ROOT = BASE_DIR / 'static'
 
 if DEBUG:
 
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+    # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 else:
 
